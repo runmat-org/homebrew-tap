294a82769acd46585ce5279242ec137cb4dd6ecc
Jonathan Weiss <gneeri@gmail.com>
2025-12-23 11:14:55 -0500

    moved version to sit next to the download urls

--- Formula/runmat.rb
+++ Formula/runmat.rb
@@ -1,14 +1,15 @@
 class Runmat < Formula
   desc "Blazing fast runtime for math workloads"
   homepage "https://runmat.org"
-  version "0.2.8"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/runmat-org/runmat/releases/download/v0.2.8/runmat-v0.2.8-macos-aarch64.tar.gz"
+      version "0.2.8"
       sha256 "72d7e63f2d4c285fbeb685aa34124380f3047218c693102989427213c54243b2"
     else
       url "https://github.com/runmat-org/runmat/releases/download/v0.2.8/runmat-v0.2.8-macos-x86_64.tar.gz"
+      version "0.2.8"
       sha256 "7f2f53d1ff8db787840af5509d94ef90bfdb11084b29a8faf84bedfed5c2ba39"
     end
   end
