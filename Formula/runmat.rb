51fc793152c34d8e9f11829c3afc6f9a2235ca67
gneeri <jweiss@dystr.com>
2026-06-13 14:20:37 -0400

    updated homebrew to version 0.5.2

--- Formula/runmat.rb
+++ Formula/runmat.rb
@@ -4,13 +4,13 @@ class Runmat < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/runmat-org/runmat/releases/download/v0.4.4/runmat-v0.4.4-macos-aarch64.tar.gz"
-      version "0.4.4"
-      sha256 "a4cd038c48c6aa493726692b3091eb1fdcc9b4129a291b176f6c528b50be8a70"
+      url "https://github.com/runmat-org/runmat/releases/download/v0.5.2/runmat-v0.5.2-macos-aarch64.tar.gz"
+      version "0.5.2"
+      sha256 "28cd748b2fcb16b18263aef3e0bdcfa4d2f2f1808e75795668e1439333e6835e"
     else
-      url "https://github.com/runmat-org/runmat/releases/download/v0.4.4/runmat-v0.4.4-macos-x86_64.tar.gz"
-      version "0.4.4"
-      sha256 "72f04b090b4595314c54ea5c73ab8648091657f05fcc24b2dd2e515a7337b5cd"
+      url "https://github.com/runmat-org/runmat/releases/download/v0.5.2/runmat-v0.5.2-macos-x86_64.tar.gz"
+      version "0.5.2"
+      sha256 "7b9b7da4ae205b527cbb473b7416ca38b408b1b704b70e33cfb9979acef63503"
     end
   end
 
